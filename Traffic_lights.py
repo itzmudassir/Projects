7d786b3a05182655566057b56ab8ba8cb4371c93
Mudassir Nadeem <itzmudassir07@gmail.com>
2022-05-23 19:59:43 +0500

    Commenting of traffic light system is done

--- Traffic_lights.py
+++ Traffic_lights.py
@@ -3,44 +3,46 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-r1 = 2
-y1 = 3
-g1 = 4
-r2 = 5
-y2 = 6
-g2 = 7
+r1 = 2  #Right Side red light
+y1 = 3  #Right Side yellow light
+g1 = 4  #Right Side green light
+r2 = 5  #Left Side red light
+y2 = 6  #Left Side yellow light
+g2 = 7  #Left Side green light
 yellow_delay = 2  #Delay for yellow light
 delay = 5 #Delay for red & green light
-GPIO.setup(r1, GPIO.OUT)
-GPIO.setup(y1, GPIO.OUT)
-GPIO.setup(g1, GPIO.OUT)
-GPIO.setup(r2, GPIO.OUT)
-GPIO.setup(y2, GPIO.OUT)
-GPIO.setup(g2, GPIO.OUT)
-GPIO.output(r1, GPIO.HIGH)
-while(True):
-    print("Right Lane")
-    GPIO.output(r1, GPIO.HIGH)
-    GPIO.output(g2, GPIO.HIGH)
-    time.sleep(delay)
-    GPIO.output(r1, GPIO.LOW)
-    GPIO.output(g2, GPIO.LOW)
-    print("Waiting for traffic light to change")
-    GPIO.output(y1, GPIO.HIGH)
-    GPIO.output(y2, GPIO.HIGH)
-    time.sleep(yellow_delay)
-    GPIO.output(y1, GPIO.LOW)
-    GPIO.output(y2, GPIO.LOW)
-    print("Left Lane")
-    GPIO.output(g1, GPIO.HIGH)
-    GPIO.output(r2, GPIO.HIGH)
-    time.sleep(delay)
-    GPIO.output(g1, GPIO.LOW)
-    GPIO.output(r2, GPIO.LOW)
-    print("Waiting for traffic light to change")
-    GPIO.output(y1, GPIO.HIGH)
-    GPIO.output(y2, GPIO.HIGH)
-    time.sleep(yellow_delay)
-    GPIO.output(y1, GPIO.LOW)
-    GPIO.output(y2, GPIO.LOW)
 
+#Set GPIO pins as output
+GPIO.setup(r1, GPIO.OUT) #Set red light as output
+GPIO.setup(y1, GPIO.OUT) #Set yellow light as output
+GPIO.setup(g1, GPIO.OUT) #Set green light as output
+GPIO.setup(r2, GPIO.OUT) #Set red light as output
+GPIO.setup(y2, GPIO.OUT) #Set yellow light as output
+GPIO.setup(g2, GPIO.OUT) #Set green light as output
+
+#Traffic light system in a loop
+while(True):
+    print("Right Lane")    #Prints the right lane
+    GPIO.output(r1, GPIO.HIGH) #Turn on red light
+    GPIO.output(g2, GPIO.HIGH) #Turn on green light
+    time.sleep(delay)  #Delay for red & green light
+    GPIO.output(r1, GPIO.LOW) #Turn off red light
+    GPIO.output(g2, GPIO.LOW) #Turn off green light
+    print("Waiting for traffic light to change")  #Prints the waiting for traffic light to change
+    GPIO.output(y1, GPIO.HIGH)  #Turn on yellow light
+    GPIO.output(y2, GPIO.HIGH)  #Turn on yellow light
+    time.sleep(yellow_delay)    #Delay for yellow light
+    GPIO.output(y1, GPIO.LOW)   #Turn off yellow light
+    GPIO.output(y2, GPIO.LOW)   #Turn off yellow light
+    print("Left Lane")   #Prints the left lane
+    GPIO.output(g1, GPIO.HIGH)   #Turn on green light
+    GPIO.output(r2, GPIO.HIGH)   #Turn on red light
+    time.sleep(delay)   #Delay for red & green light
+    GPIO.output(g1, GPIO.LOW)   #Turn off green light
+    GPIO.output(r2, GPIO.LOW)   #Turn off red light
+    print("Waiting for traffic light to change")  #Prints the waiting for traffic light to change
+    GPIO.output(y1, GPIO.HIGH) #Turn on yellow light
+    GPIO.output(y2, GPIO.HIGH) #Turn on yellow light
+    time.sleep(yellow_delay)  #Delay for yellow light
+    GPIO.output(y1, GPIO.LOW) #Turn off yellow light
+    GPIO.output(y2, GPIO.LOW) #Turn off yellow light
